a1cf69eccff1abc98c9ae26d56c2c32b042ab406
Joshua Lee <yuloklee@hotmail.com>
2020-05-25 17:21:19 -0700

    Improve mobile compatability

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,7 +27,7 @@ const IndexPage = () => {
   return (
     <div>
       <Helmet>
-          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
           <title>Joshua Lee</title>
           <link rel="canonical" href="https://yulok.me/" />
         </Helmet>
